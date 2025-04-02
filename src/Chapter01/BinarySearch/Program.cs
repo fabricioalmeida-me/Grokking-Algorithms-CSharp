2ed2247a0e2b8cad3c4d705fb7b18b8b00838702
Fabricio Almeida <fabricioalmeida.me@gmail.com>
2025-04-02 20:04:53 -0300

    Implemented Tests in Binary Search algorithm and refactored the name project.

---
+++ src/Chapter01/BinarySearch/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BinarySearch;
+
+public class Program
+{
+  public static void Main(string[] args)
+  {
+    var number = 6;
+    var myList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    var index = BinarySearchAlgorithm.Search(myList, number);
+    if (index.HasValue)
+    {
+      Console.WriteLine($"The number {number} was found at index {index}.");
+    }
+    else
+    {
+      Console.WriteLine("The number was not found in the list.");
+    }
+  }
+}
---
+++ tests/Chapter01/BinarySearchTests/BinarySearchAlgorithmTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+using BinarySearch;
+
+namespace BinarySearchTests;
+
+public class BinarySearchAlgorithmTests
+{
+    [Fact]
+    public void Search_FindsExistingNumber_ReturnsCorrectIndex()
+    {
+        // Arrange
+        var myList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+        var number = 13;
+
+        // Act
+        var result = BinarySearchAlgorithm.Search(myList, number);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(12, result);
+    }
+
+    [Fact]
+    public void Search_NumberNotInList_ReturnsNull()
+    {
+        //Arrange
+        var myList = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        var number = 13;
+
+        //Act
+        var result = BinarySearchAlgorithm.Search(myList, number);
+
+        //Assert
+        Assert.Null(result);
+    }
+}
