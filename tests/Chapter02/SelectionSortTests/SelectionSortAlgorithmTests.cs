895fe9d9dab32ffac64a0acb98477d1904660d72
Fabricio Almeida <fabricioalmeida.me@gmail.com>
2025-04-09 15:55:56 -0300

    Implemented tests for the Selection Sort algorithm.

---
+++ tests/Chapter02/SelectionSortTests/SelectionSortAlgorithmTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using SelectionSort;
+
+namespace SelectionSortTests;
+
+public class SelectionSortAlgorithmTests
+{
+    [Fact]
+    public void ShouldReturnSortedArray()
+    {
+        //Arrange
+        var inputArray = new List<int> { 10, 5, 3, 6, 2 };
+        var expectedArray = new List<int> { 2, 3, 5, 6, 10 };
+        //Act
+        var result = SelectionSortAlgorithm.SortList(inputArray);
+
+        //Assert
+        Assert.Equal(expectedArray, result);
+    }
+}
