13a455c487872a72169023882b39d7b0188f96d1
Fabricio Almeida <fabricioalmeida.me@gmail.com>
2025-04-21 12:01:43 -0300

    Implement tests for loop and recursive sum algorithms.

---
+++ tests/Chapter04/DivideAndConquer/LoopSumTests/LoopSumAlgorithmTests.cs
@@ -0,0 +1,19 @@
+﻿using LoopSum;
+
+namespace LoopSumTests;
+
+public class LoopSumAlgorithmTests
+{
+    [Fact]
+    public void Should_ReturnCorrectSum()
+    {
+        //Arrange
+        var inputList = new List<int> { 1, 2, 3, 4, 5 };
+        var expectedResult = 15;
+        //Act
+        var result = LoopSumAlgorithm.Sum(inputList);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ tests/Chapter04/DivideAndConquer/RecursiveSumTests/RecursiveSumAlgorithmTests.cs
@@ -0,0 +1,20 @@
+﻿using RecursiveSum;
+
+namespace RecursiveSumTests;
+
+public class RecursiveSumAlgorithmTests
+{
+    [Fact]
+    public void Should_ReturnCorrectSum()
+    {
+        //Arrange
+        var inputList = new List<int> { 1, 2, 3, 4, 5 };
+        var expectedResult = 15;
+
+        //Act
+        var result = RecursiveSumAlgorithm.Sum(inputList);
+
+        //Assert
+        Assert.Equal(expectedResult, result);
+    }
+}
