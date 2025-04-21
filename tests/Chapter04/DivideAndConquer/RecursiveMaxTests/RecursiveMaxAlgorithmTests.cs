de854aeeb10d585f69a011e51f520a6d2df715cc
Fabricio Almeida <fabricioalmeida.me@gmail.com>
2025-04-21 16:56:12 -0300

    Implement tests for recursive max algorithms.

---
+++ tests/Chapter04/DivideAndConquer/RecursiveMaxTests/RecursiveMaxAlgorithmTests.cs
@@ -0,0 +1,20 @@
+﻿using RecursiveMax;
+
+namespace RecursiveMaxTests;
+
+public class RecursiveMaxAlgorithmTests
+{
+    [Fact]
+    public void MaxShouldReturnCorrectMaximumNumber()
+    {
+        //Arrange
+        var inputNumbers = new List<int> { 3, 1, 10, 7, 4, 9 };
+        var expectedMaxNumber = 10;
+
+        //Act
+        var result = RecursiveMaxAlgorithm.Max(inputNumbers);
+
+        //Assert
+        Assert.Equal(expectedMaxNumber, result);
+    }
+}
