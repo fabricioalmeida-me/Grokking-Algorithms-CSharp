6480faaba8b5020f4be02b1169c986719557ebef
Fabricio Almeida <fabricioalmeida.me@gmail.com>
2025-05-05 17:50:15 -0300

    Implement Dictionary for grocery prices with tests.

---
+++ src/Chapter05/HashTable/PriceOfGroceries/Program.cs
@@ -0,0 +1,9 @@
+﻿var book = new Dictionary<string, decimal>();
+book.Add("Apple", 0.67m);
+book.Add("Milk", 1.50m);
+book.Add("Abacate", 1.49m);
+
+foreach (var item in book)
+{
+  Console.WriteLine("{0}: {1}", item.Key, item.Value);
+}
---
+++ tests/Chapter05/HashTable/PriceOfGroceriesTests/PriceOfGroceriesAlgorithmTests.cs
@@ -0,0 +1,21 @@
+﻿namespace PriceOfGroceriesTests;
+
+public class PriceOfGroceriesAlgorithmTests
+{
+    [Fact]
+    public void Should_Contain_Expected_Items()
+    {
+        //Arrange
+        var book = new Dictionary<string, decimal>();
+        book.Add("Apple", 0.67m);
+        book.Add("Milk", 1.50m);
+        book.Add("Abacate", 1.49m);
+
+        //Assert
+        Assert.Equal(3, book.Count);
+        Assert.Equal(0.67m, book["Apple"]);
+        Assert.Equal(1.50m, book["Milk"]);
+        Assert.Equal(1.49m, book["Abacate"]);
+        Assert.False(book.ContainsKey("Banana"));
+    }
+}
