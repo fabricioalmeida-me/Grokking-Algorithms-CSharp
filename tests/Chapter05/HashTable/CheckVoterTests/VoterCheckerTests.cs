cd9233102380890b5705016cfddcf7c5663ddbbe
Fabricio Almeida <fabricioalmeida.me@gmail.com>
2025-05-05 19:21:51 -0300

    Implement voter check tests.

---
+++ tests/Chapter05/HashTable/CheckVoterTests/VoterCheckerTests.cs
@@ -0,0 +1,39 @@
+﻿using CheckVoter;
+
+namespace CheckVoterTests;
+
+public class VoterCheckerTests
+{
+    public VoterCheckerTests()
+    {
+        VoterChecker.ClearVotes();
+    }
+
+    [Fact]
+    public void Should_Allow_First_Time_Voter()
+    {
+        //Arrange
+        var name = "Fabrício";
+
+        //Act
+        bool result = VoterChecker.RegisterVote(name);
+
+        //Assert
+        Assert.True(result);
+        Assert.True(VoterChecker.HasVoted(name));
+    }
+
+    [Fact]
+    public void Should_Deny_Second_Vote()
+    {
+        //Arrange
+        var name = "Ketlen";
+        VoterChecker.RegisterVote(name);
+
+        //Act
+        bool result = VoterChecker.RegisterVote(name);
+
+        //Assert
+        Assert.False(result);
+    }
+}
